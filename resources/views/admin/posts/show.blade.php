f8711495138b8b240e83fb26510effbe96c70b07
matteo9723 <matteolandri7@gmail.com>
2022-02-03 23:25:00 +0100

    aggiunto container a show.blade.php

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="container">
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">titolo</th>
-      <th scope="col">contenuto</th>
-      <th scope="col">slug</th>
-    </tr>
-  </thead>
-  <tbody>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">titolo</th>
+        <th scope="col">contenuto</th>
+        <th scope="col">slug</th>
+      </tr>
+    </thead>
+    <tbody>
+  
+      <tr>
+        <th scope="row">{{$post->id}}</th>
+        <td>{{$post->title}}</td>
+        <td>{{$post->content}}</td>
+        <td>{{$post->slug}}</td>
+      </tr>
+   
+    </tbody>
+  </table>
+  
+  <a href="{{route('admin.posts.index')}}" class="link-primary">torna indietro</a>
+</div>
 
-    <tr>
-      <th scope="row">{{$post->id}}</th>
-      <td>{{$post->title}}</td>
-      <td>{{$post->content}}</td>
-      <td>{{$post->slug}}</td>
-    </tr>
- 
-  </tbody>
-</table>
-
-<a href="{{route('admin.posts.index')}}" class="link-primary">torna indietro</a>
 
 
   
